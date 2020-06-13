bef357ef21b9fe0d851787a9e3928db0e909fee4
Floaddy <mateus-pevidor@hotmail.com>
2020-06-13 05:46:12 -0300

    Add vehicles storage

---
+++ src/database/VehiclesDatabase.ts
@@ -0,0 +1,90 @@
+import fs from 'fs';
+import path from 'path';
+import { uuid } from 'uuidv4';
+
+interface Vehicle {
+  id: string;
+  model: string;
+  yearOfManufacture: number;
+  doorCount: 0 | 4;
+  brand: string;
+}
+
+export class Car implements Vehicle {
+  id: string = uuid();
+
+  model: string;
+
+  yearOfManufacture: number;
+
+  doorCount: 4 = 4;
+
+  brand: string;
+
+  constructor({
+    model,
+    yearOfManufacture,
+    brand,
+  }: Omit<Car, 'id' | 'doorCount'>) {
+    this.model = model;
+    this.yearOfManufacture = yearOfManufacture;
+    this.brand = brand;
+  }
+}
+
+export class Bike implements Vehicle {
+  id: string = uuid();
+
+  model: string;
+
+  yearOfManufacture: number;
+
+  doorCount: 0 = 0;
+
+  wheelCount = 2;
+
+  passengers: 1 | 2;
+
+  brand: string;
+
+  constructor({
+    model,
+    yearOfManufacture,
+    passengers,
+    brand,
+  }: Omit<Bike, 'id' | 'doorCount' | 'wheelCount'>) {
+    this.model = model;
+    this.yearOfManufacture = yearOfManufacture;
+    this.passengers = passengers;
+    this.brand = brand;
+  }
+}
+
+export default class VehiclesDatabase {
+  public read(): Array<Car | Bike> {
+    const data = fs.readFileSync(
+      path.resolve(__dirname, 'vehicles.json'),
+      'utf8',
+    );
+    const vehicles = JSON.parse(data) as Array<Car | Bike>;
+
+    return vehicles;
+  }
+
+  public save(vehicle: Car | Bike): Car | Bike {
+    const data = fs.readFileSync(
+      path.resolve(__dirname, 'vehicles.json'),
+      'utf8',
+    );
+    const vehicles = JSON.parse(data) as Array<Car | Bike>;
+
+    vehicles.push(vehicle);
+
+    fs.writeFileSync(
+      path.resolve(__dirname, 'vehicles.json'),
+      JSON.stringify(vehicles),
+    );
+
+    return vehicle;
+  }
+}
